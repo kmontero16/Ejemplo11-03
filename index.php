154f66256d32595f8e1a6ed0172f2404619f37ce
cetecom <cetecom@LCV10819>
2019-03-11 22:28:37 -0300

    Ejemplo de posicion div cos css

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<link href="Estilos.css" rel="stylesheet" type="text/css"/>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div id="menu" >
+            div1
+        </div>
+        <div id="head">
+            div2
+        </div>
+        <div id="contenedor">
+            
+            div3
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
